ce627d3f9078eaf374dbb44f724a5db26698288a
Jacquelynne Heiman <jacheima@uat.edu>
2019-03-13 16:23:43 -0700

    I got the animations working thanks to will!!! Now i have to get him to face the direction hes moving.

--- SuperUATBros/Assets/Scripts/PlayerController.cs
+++ SuperUATBros/Assets/Scripts/PlayerController.cs
@@ -6,13 +6,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed = 1f;
+    public float Speed = 1f;
     private bool isJumping = false;
     public float maxJumpTimes = 2;
     public float jumpHeight = 12f;
 
     private Rigidbody2D rb;
     public float someScale = 3;
+
+    public Animator anim;
 
 
 	// Use this for initialization
@@ -35,18 +37,13 @@ public class PlayerController : MonoBehaviour
             rb.AddForce(Vector3.up * jumpHeight, ForceMode2D.Impulse);
         }
 
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.localScale = new Vector2(-someScale, transform.localScale.y);
-            transform.position += -Vector3.right * Time.deltaTime * speed;
-        }
+            float velocityX = Input.GetAxis("Horizontal");
+            float velocityY = rb.velocity.y;
 
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.localScale = new Vector2(someScale, transform.localScale.y);
-            transform.position += Vector3.right * Time.deltaTime * speed;
-        }
+            rb.velocity = new Vector2(velocityX * Speed, velocityY);
 
+        anim.SetFloat("Speed", Mathf.Abs(velocityX));
+  
     }
 
     void OnCollisionEnter2D(Collision2D collision)
