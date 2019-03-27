02ed11f3cda93b03dbfa8f8893a3ddd132e101c3
Jacquelynne Heiman <jacheiman@uat.edu>
2019-03-27 11:48:29 -0700

    I got level 3 built and working

---
+++ SuperUATBros/Assets/Scripts/LoadLevels.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevels : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadLevelOne()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void LoadLevelTwo()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+    public void LoadLevelThree()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+
+    public void LoadLevelFour()
+    {
+        SceneManager.LoadScene("Level4");
+    }
+
+    public void LoadLevelFive()
+    {
+        SceneManager.LoadScene("Level5");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
